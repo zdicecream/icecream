7bac5fe695e98f74e9c0a3f0a8c4544577dbd0b1
zdicecream <zhaoding987@163.com>
2023-08-04 10:04:56 +0800

    2023 调整架构

--- src/test/java/com/zdinit/icecream/Transation2Test.java
+++ src/test/java/com/zdinit/icecream/Transation2Test.java
@@ -1,6 +1,8 @@
 package com.zdinit.icecream;
 
+import com.zdinit.icecream.auth.entity.Runstate;
 import com.zdinit.icecream.auth.entity.User;
+import com.zdinit.icecream.auth.service.IRunstateService;
 import com.zdinit.icecream.auth.service.IUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class Transation2Test {
     @Autowired
-    private IUserService userService;
+    private IRunstateService runstateService;
     @Autowired
     private Transation3Test transation3Test;
-    @Transactional
     public void server2(){
-        User user = new User();
-        user.setShowName("1122");
-        userService.saveUser(user);
+        Runstate runstate = new Runstate();
+        runstate.setId(22L);
+        runstateService.save(runstate);
 
         transation3Test.server3();
+
     }
 }
--- src/test/java/com/zdinit/icecream/Transation3Test.java
+++ src/test/java/com/zdinit/icecream/Transation3Test.java
@@ -1,20 +1,22 @@
 package com.zdinit.icecream;
 
+import com.zdinit.icecream.auth.entity.Runstate;
 import com.zdinit.icecream.auth.entity.User;
+import com.zdinit.icecream.auth.service.IRunstateService;
 import com.zdinit.icecream.auth.service.IUserService;
+import org.apache.ibatis.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 @Service
 public class Transation3Test {
     @Autowired
-    private IUserService userService;
-    @Transactional
+    private IRunstateService runstateService;
     public void server3(){
-        User user = new User();
-        user.setShowName("112233");
-        userService.saveUser(user);
-        throw new RuntimeException("11");
+        Runstate runstate = new Runstate();
+        runstate.setId(33L);
+        runstateService.save(runstate);
     }
 }
